7a3c9ec21f7d75d539680a14653dfdf7f33dda3f
Stepan Smirnov <stechkin1986@gmail.com>
2020-10-25 17:57:44 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float Ticketprice = 317700.00f;
+        float pricebonus = 20.00f;
+        float mile = Ticketprice / pricebonus;
+        System.out.println(mile);
+    }
+
+}
